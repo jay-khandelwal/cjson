673295fb1b6f501e80819300010ec50551e4617d
jay-ny <100901219+jay-ny@users.noreply.github.com>
2023-04-02 16:19:37 +0530

    added pre-commit

--- include/json_token.h
+++ include/json_token.h
@@ -8,51 +8,43 @@
 #define debug_print_string_from_input(...)
 #endif
 
-typedef enum token_type_t
-{
-    TOKEN_TYPE_NONE,
-    TOKEN_TYPE_OBJECT_BEGIN,
-    TOKEN_TYPE_OBJECT_END,
-    TOKEN_TYPE_ARRAY_START,
-    TOKEN_TYPE_ARRAY_END,
-    TOKEN_TYPE_STRING,
-    TOKEN_TYPE_NUMBER,
-    TOKEN_TYPE_TRUE,
-    TOKEN_TYPE_FALSE,
-    TOKEN_TYPE_NULL,
-    TOKEN_TYPE_COMMA,
-    TOKEN_TYPE_COLON,
+typedef enum token_type_t {
+  TOKEN_TYPE_NONE,
+  TOKEN_TYPE_OBJECT_BEGIN,
+  TOKEN_TYPE_OBJECT_END,
+  TOKEN_TYPE_ARRAY_START,
+  TOKEN_TYPE_ARRAY_END,
+  TOKEN_TYPE_STRING,
+  TOKEN_TYPE_NUMBER,
+  TOKEN_TYPE_TRUE,
+  TOKEN_TYPE_FALSE,
+  TOKEN_TYPE_NULL,
+  TOKEN_TYPE_COMMA,
+  TOKEN_TYPE_COLON,
 
 } token_type_t;
 
-typedef enum token_state_t
-{
-    TOKEN_STATE_START,
-    TOKEN_STATE_KEY,
-    TOKEN_STATE_KEY_END,
-    TOKEN_STATE_VALUE,
-    TOKEN_STATE_VALUE_END,
-    TOKEN_STATE_END,
+typedef enum token_state_t {
+  TOKEN_STATE_START,
+  TOKEN_STATE_KEY,
+  TOKEN_STATE_KEY_END,
+  TOKEN_STATE_VALUE,
+  TOKEN_STATE_VALUE_END,
+  TOKEN_STATE_END,
 } token_state_t;
 
-typedef enum container_t
-{
-    CONTAINER_OBJECT,
-    CONTAINER_ARRAY
-} container_t;
+typedef enum container_t { CONTAINER_OBJECT, CONTAINER_ARRAY } container_t;
 
-typedef struct container_depth_t
-{
-    int depth;
-    container_t container;
-    struct container_depth_t *parent;
+typedef struct container_depth_t {
+  int depth;
+  container_t container;
+  struct container_depth_t *parent;
 } container_depth_t;
 
-typedef struct json_token_t
-{
-    token_type_t type;
-    char *start;
-    char *end;
+typedef struct json_token_t {
+  token_type_t type;
+  char *start;
+  char *end;
 } json_token_t;
 
 json_token_t *tokenize(char *input);
@@ -65,6 +57,8 @@ int get_string_length(char *input, int is_key);
 int get_number_length(char *input);
 int compare_value_from_input(char *input, int counter, char cmp_value[]);
 void print_string_from_input(char *input, int string_len);
-container_depth_t *get_new_container_depth(container_t container, container_depth_t *parent);
-container_depth_t *get_container_depth_parent(container_depth_t *container_depth);
+container_depth_t *get_new_container_depth(container_t container,
+                                           container_depth_t *parent);
+container_depth_t *
+get_container_depth_parent(container_depth_t *container_depth);
 void print_container_depth(container_depth_t *container_depth);
--- src/main.c
+++ src/main.c
@@ -1,19 +1,17 @@
 #include "json_token.h"
 #include "utils.h"
 
-int main()
-{
-    char *buffer = read_file("helper/json_files/big_chunk.json");
+int main() {
+  char *buffer = read_file("helper/json_files/big_chunk.json");
 
-    json_token_t *tokens = tokenize(buffer);
+  json_token_t *tokens = tokenize(buffer);
 
-    if  (tokens== NULL)
-    {
-        perror("Invalid JSON");
-        exit(EXIT_FAILURE);
-    }
+  if (tokens == NULL) {
+    perror("Invalid JSON");
+    exit(EXIT_FAILURE);
+  }
 
-    free(tokens);
-    free(buffer);
-    return 0;
+  free(tokens);
+  free(buffer);
+  return 0;
 }
